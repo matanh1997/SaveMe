12bea4ab74314d8cb6ffcc3c6a1b0ca22d3c6008
Matan Heled <matanh1997@gmail.com>
2020-10-08 23:42:06 +0300

    Made it so /audio can accept safe_line input

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
+app = Flask(__name__)
 import speech_recognition as sr
 r = sr.Recognizer()
-app = Flask(__name__)
+
+import copy
 
 SAVE_PATH = 'C:/Users/Matan/SaveMe/harvard.wav'
 RECORDING_DICT = {
@@ -21,15 +23,16 @@ def get_audio_file():
                }, 200
     if request.method == 'POST':
         if request.files['file']:
+            safe_line = return_safe_words_arr(request)
             # saving file to FS
             save_file_from_request(request)
             audio = get_audio_from_file()
             text: str = translate_audio_to_text(audio)
             RECORDING_DICT['CURR_RECORDING'].append(text.split(' '))
             RECORDING_DICT['NUM_RECORDING'] += 1
-            check_recording_for_sequence()
             if RECORDING_DICT['NUM_RECORDING'] >= 2:
-                if 'SHOULD_PING' in RECORDING_DICT:
+                if check_recording_for_sequence(safe_line):
+                    print("FOUND SEQUENCE!")
                     return {
                                'message': 'emergency',
                                'action': 'emualate call'
@@ -54,17 +57,17 @@ def translate_audio_to_text(audio):
     return r.recognize_google(audio)
 
 
-def check_recording_for_sequence():
+def check_recording_for_sequence(safe_line):
     first_rec = RECORDING_DICT['CURR_RECORDING'][RECORDING_DICT['NUM_RECORDING'] - 2]
     second_rec = RECORDING_DICT['CURR_RECORDING'][RECORDING_DICT['NUM_RECORDING'] - 1]
-
-    found, where = search_first_word(first_rec, second_rec)
-    if found and where == 1:
-        if search_second_word_in_both(first_rec, second_rec):
-            RECORDING_DICT['SHOULD_PING'] = True
-    elif found and where == 2:
-        if search_second_word_in_second(second_rec):
-            RECORDING_DICT['SHOULD_PING'] = True
+    return search_safe_line_sequence(safe_line, first_rec, second_rec)
+    # found, where = search_first_word(first_rec, second_rec)
+    # if found and where == 1:
+    #     if search_second_word_in_both(first_rec, second_rec):
+    #         RECORDING_DICT['SHOULD_PING'] = True
+    # elif found and where == 2:
+    #     if search_second_word_in_second(second_rec):
+    #         RECORDING_DICT['SHOULD_PING'] = True
 
 
 def search_first_word(first_rec, second_rec):
@@ -96,6 +99,25 @@ def search_second_word_in_second(second_rec):
         if word == SECOND_SAFE_WORD:
             return True
     return False
+
+def return_safe_words_arr(flask_req):
+    req_safe_line = flask_req.form['safe_line']
+    return req_safe_line.split(' ')
+
+def search_safe_line_sequence(safe_line, first_rec, second_rec):
+    safe_line_tracking = copy.copy(safe_line)
+    for safe_word in safe_line:
+        if safe_word in first_rec:
+            safe_line_tracking.remove(safe_word)
+            continue
+        if safe_word in second_rec:
+            safe_line_tracking.remove(safe_word)
+    if len(safe_line_tracking) == 0:
+        return True
+    else:
+        return False
+
+
 
 
 if __name__ == '__main__':
