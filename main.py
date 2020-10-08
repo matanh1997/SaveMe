62ea17f46cdb2ae71524484fc906be2a9a10e857
Matan Heled <matanh1997@gmail.com>
2020-10-09 00:17:52 +0300

    added requirements.txt

--- main.py
+++ main.py
@@ -121,4 +121,4 @@ def search_safe_line_sequence(safe_line, first_rec, second_rec):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
